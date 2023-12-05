479ac80883eaaa839193ba6e38809eb6a9b9af10
Lior1254 <liorshemtov2008@gmail.com>
2023-12-05 09:10:20 +0200

    its work

--- app/src/main/java/com/example/hw1_albert_5_12_2023/MainActivity.java
+++ app/src/main/java/com/example/hw1_albert_5_12_2023/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.hw1_albert_5_12_2023;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+
+    }
+
+    public void lol(View view) {
+        btn.setText("Oh, yea, I’ve been clicked!");
     }
 }
